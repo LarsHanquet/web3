fc2c1bd53f3cde5befc4e357d7df0aa17fc95884
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2024-09-17 15:20:01 +0200

    new:ex2.1-init

---
+++ exercises/module2/2.1/src/components/ClickCounter.jsx
@@ -0,0 +1,16 @@
+const ClickCounter = ({count, setCount}) => {
+    return (
+        <>
+        <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+        </>
+    )
+}
+
+export default ClickCounter
