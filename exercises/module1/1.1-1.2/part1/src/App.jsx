7e21fcaa125a70a4e37cfd63e38becc20d440dc1
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2024-09-17 14:43:04 +0200

    1.1-1.2 : refactor - absolute imports

--- exercises/module1/1.1-1.2/part1/src/App.jsx
+++ exercises/module1/1.1-1.2/part1/src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "../Header/Header"
-import Footer from "../Footer/Footer"
-import Content from "../Content/Content"
+import Header from "/Header/Header"
+import Footer from "/Footer/Footer"
+import Content from "/Content/Content"
 
 const App = () => {
   const course = 'Half Stack application development'
