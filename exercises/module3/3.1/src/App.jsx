7b1898c60e5fb3dd4823d32845293cd4e7bcc6a5
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2024-10-01 14:46:11 +0200

    module3 : loading

--- exercises/module3/3.1/src/App.jsx
+++ exercises/module3/3.1/src/App.jsx
@@ -1,4 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+
+const Loading = () => {
+  return (
+    <>
+      <h1>Loading...</h1>
+    </>
+  );
+};
 
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + bad + neutral;
@@ -9,17 +17,18 @@ const Statistics = ({ good, neutral, bad }) => {
     <>
       <h1>statistics</h1>
       <table>
-      <StatisticsLine text="good" value={good} />
-      <StatisticsLine text="neutral" value={neutral} />
-      <StatisticsLine text="bad" value={bad} />
-      <StatisticsLine text="all" value={all} />
-      <StatisticsLine text="average" value={average} />
-      <StatisticsLine text="positive" value={`${positive} %`} />
+        <tbody>
+          <StatisticsLine text="good" value={good} />
+          <StatisticsLine text="neutral" value={neutral} />
+          <StatisticsLine text="bad" value={bad} />
+          <StatisticsLine text="all" value={all} />
+          <StatisticsLine text="average" value={average} />
+          <StatisticsLine text="positive" value={`${positive} %`} />
+        </tbody>
       </table>
-
     </>
-  )
-}
+  );
+};
 
 const StatisticsLine = ({ text, value }) => {
   return (
@@ -27,22 +36,30 @@ const StatisticsLine = ({ text, value }) => {
       <td>{text}</td>
       <td>{value}</td>
     </tr>
-
-  )
-}
+  );
+};
 
 const Button = ({ update, text }) => {
   return (
-    <button onClick={() => update(prev => prev + 1)}>
+    <button onClick={() => update((prev) => prev + 1)}>
       {text}
     </button>
-  )
-}
+  );
+};
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // 3 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div>
@@ -51,13 +68,15 @@ const App = () => {
       <Button update={setNeutral} text="neutral" />
       <Button update={setBad} text="bad" />
 
-      {good + neutral + bad !== 0 ? (
+      {loading ? (
+        <Loading />
+      ) : good + neutral + bad !== 0 ? (
         <Statistics good={good} neutral={neutral} bad={bad} />
       ) : (
         <p>No feedback given</p>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
